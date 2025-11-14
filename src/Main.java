4a83837d97e712619f2f09aac3777824b2fc05b9
OsbornChen <osbornc@nycstudents.net>
2025-11-14 11:49:13 -0500

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println();
+    }
+    public static int read() throws FileNotFoundException {
+        File f = new File("Guesses.txt");
+        Scanner s = new Scanner(f);
+        int counter = 0;
+        while (s.hasNext()) {
+            while (s.nextInt() != 0){
+
+            }
+
+        }
+        return counter;
+    }
 }
